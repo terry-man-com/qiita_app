0ac6435727cb580b63b814cfc1ede150e0e31591
terry-man-com <terry.man.com@icloud.com>
2022-12-09 21:39:44 +0900

    qiitaapiのenvとconfigを設定

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\ArticleController::class, 'index']);
+
+Route::resource('/articles', App\Http\Controllers\ArticleController::class);
